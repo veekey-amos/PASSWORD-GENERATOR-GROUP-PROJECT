ee2234bc0850837d754299638c509db1b71a2a4e
veekey-amos <150281777+veekey-amos@users.noreply.github.com>
2024-01-28 13:41:58 -0800

    Add files via upload

---
+++ welcome label.py
@@ -0,0 +1,2 @@
+welcome_label = Label(root, text="Welcome! Click the Generate button to generate your unique password.", font=("Arial bold", 12, "bold"), bg="black", fg="white")
+welcome_label.pack(pady=10)

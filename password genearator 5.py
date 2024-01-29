4eb8a533a9600505c17ea2f0e79ebd9cf98a15af
veekey-amos <150281777+veekey-amos@users.noreply.github.com>
2024-01-29 15:17:46 -0800

    Update password generator 5.py

--- password genearator 5.py
+++ password genearator 5.py
@@ -3,5 +3,3 @@ ny_frame.pack(pady=20)
 
 ny_button = Button(ny_frame, text="Generate unique password", command=new_rand, bg="black", fg="white")
 ny_button.grid(row=0, column=0, padx=10)
-
-root.mainloop()

f301935691755591e90de0ee37eda56795c7a1b6
Marvydara <149010152+Marvydara@users.noreply.github.com>
2024-01-29 10:35:27 -0800

    Add files via upload

---
+++ password_generation.py
@@ -0,0 +1,7 @@
+ny_entry = Entry( font=("Arial bold",44),bg ="white",fg ="midnightblue")
+ny_entry.pack(pady=20)
+
+pw_entry = Entry(root, text='', font=('Arial bold', 24), bd=0, bg="white",
+fg="midnightblue")
+pw_entry.pack(pady=20)
+
